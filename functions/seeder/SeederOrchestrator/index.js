c6ae8e73dee67b23a92c8a419a49d49c7084c197
Hugo Girard <hugogirard28@hotmail.com>
2021-08-14 14:18:14 -0400

    fist commit

---
+++ functions/seeder/SeederOrchestrator/index.js
@@ -0,0 +1,11 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    outputs.push(yield context.df.callActivity("UploadInvoices", "Tokyo"));
+    outputs.push(yield context.df.callActivity("UploadInvoices", "Seattle"));
+    outputs.push(yield context.df.callActivity("UploadInvoices", "London"));
+
+    return outputs;
+});
